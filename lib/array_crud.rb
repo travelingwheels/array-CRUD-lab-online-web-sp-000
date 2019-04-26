4f1bf9625ae6bf415bddf249ea509333f27db111
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-26 01:25:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["shoes","socks","hair gel"]
+  << "clams"
 end
 
 def add_element_to_start_of_array(array, element)
